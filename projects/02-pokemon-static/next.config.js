dcb77fff7a127c4e390325ddd54c1c4c8607948a
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-06-30 15:25:31 -0600

    chore(projects/02-pokemon-static): Add next config to the page

--- projects/02-pokemon-static/next.config.js
+++ projects/02-pokemon-static/next.config.js
@@ -1,6 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  /* Usamos el objeto images para que Next.js sepa que dominios de 
+  imágenes nos van a permitir cargarlas. Ya que siempre Next trata de 
+  renderizar las images de nuestro dominio local, en la carpeta public, y
+  si no la encuentra ahi va a verificar que este en este arreglo. */
+  images: {
+    domains: ['raw.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
