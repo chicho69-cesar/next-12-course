9335056355ab609e24b666c042311529dda9bf7b
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-06 15:46:06 -0600

    refactor(projects/03-open-jira): Refactor the middleware function on section 9

--- projects/03-open-jira/middleware.ts
+++ projects/03-open-jira/middleware.ts
@@ -5,18 +5,25 @@ import type { NextRequest } from 'next/server'
 export function middleware(req: NextRequest) {
   // console.log(req.nextUrl)
 
+  /* Validamos la ruta a la cual queremos aplicar las siguientes validaciones del middleware */
   if (req.nextUrl.pathname.startsWith('/api/entries/')) {
-    // const id = req.nextUrl.pathname.split('/').pop()
-    const id = req.nextUrl.pathname.replace('/api/entries/', '')
-    
-    const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$")
-    if (!checkMongoIDRegExp.test(id)) {
-      const url = req.nextUrl.clone()
-      url.pathname = '/api/bad-request'
-      url.search = `?message=This id: ${id} is not a valid MongoID`
+    return validateMongoID(req)
+  }
 
-      return NextResponse.rewrite(url)
-    }
+  return NextResponse.next()
+}
+
+function validateMongoID(req: NextRequest): NextResponse {
+  // const id = req.nextUrl.pathname.split('/').pop()
+  const id = req.nextUrl.pathname.replace('/api/entries/', '')
+  
+  const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$")
+  if (!checkMongoIDRegExp.test(id)) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/api/bad-request'
+    url.search = `?message=This id: ${id} is not a valid MongoID`
+
+    return NextResponse.rewrite(url)
   }
 
   return NextResponse.next()
